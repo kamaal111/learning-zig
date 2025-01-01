e19388dae88652c6cf9b12b638f25c2597e6bea8
Kamaal Farah <kamaal.f1@gmail.com>
2025-01-01 17:04:48 +0100

    Functions

--- src/main.zig
+++ src/main.zig
@@ -125,3 +125,24 @@ test "for" {
 
     for (string) |_| {}
 }
+
+fn addFive(x: u16) u32 {
+    return x + 5;
+}
+
+test "function" {
+    const y = addFive(10);
+    try expect(@TypeOf(y) == u32);
+    try expect(@TypeOf(y) != u16);
+    try expect(y == 15);
+}
+
+fn fibonacci(n: u16) u16 {
+    if (n == 0 or n == 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+test "function recursion" {
+    const x = fibonacci(10);
+    try expect(x == 55);
+}
